4199ab275023ab7aa0c3f57a1041c9701ba170c4
Kaloyan Nikolov <kknikolov8@gmail.com>
2024-02-22 15:36:02 +0200

    created "CustomStack" and "CustomQueue" classes.

--- skeleton/src/queue.py
+++ skeleton/src/queue.py
@@ -1,6 +1,41 @@
-
 from src.linked_list_node import LinkedListNode
 
 
 class CustomQueue:
-    pass
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self._count = 0
+
+    def enqueue(self, value):
+        new_node = LinkedListNode(value)
+        if self.is_empty:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self._count += 1
+
+    def dequeue(self):
+        if self.is_empty:
+            raise ValueError("Cannot dequeue from an empty queue")
+        value = self.head.value
+        self.head = self.head.next
+
+        self._count -= 1
+
+        return value
+
+    def peek(self):
+        if self.is_empty:
+            raise ValueError("Cannot peek into an empty queue")
+        return self.head.value
+
+    @property
+    def is_empty(self):
+        return self._count == 0
+
+    @property
+    def count(self):
+        return self._count
--- skeleton/src/stack.py
+++ skeleton/src/stack.py
@@ -3,4 +3,34 @@ from src.linked_list_node import LinkedListNode
 
 
 class CustomStack:
-    pass
+    def __init__(self):
+        self.head = None
+        self._count = 0
+
+    def push(self, value):
+        new_node = LinkedListNode(value)
+        self.head = new_node
+        self._count += 1
+
+    def pop(self):
+        if self.is_empty:
+            raise ValueError("Cannot pop from an empty stack")
+        value = self.head.value
+        self.head = self.head.next
+        self._count -= 1
+
+        return value
+
+    def peek(self):
+        if self.is_empty:
+            raise ValueError("Cannot peek into an empty queue")
+        return self.head.value
+
+    @property
+    def is_empty(self):
+        return self._count == 0
+
+    @property
+    def count(self):
+        return self._count
+
